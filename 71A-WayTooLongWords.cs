e2abefda67c36d3560a1ff1f60b7b867d81c1e3b
Md. Mehedi Hasan <mehedi53423@gmail.com>
2023-01-28 02:04:44 +0600

    71A - Way Too Long Words

---
+++ 71A-WayTooLongWords.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace WayTooLongWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt16(Console.ReadLine());
+
+            while(n > 0)
+            {
+                string w = Console.ReadLine();
+
+                if (w.Length > 10)
+                {
+                    var c = w.Length;
+                    Console.WriteLine("" + w[0] + (c - 2) + w[c - 1]);
+                }
+                else
+                {
+                    Console.WriteLine(w);
+                }
+                n--;
+            }
+        }
+    }
+}
