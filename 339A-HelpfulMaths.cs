41ac20ae7898c29f414efec5537d4693209ad5b1
Md. Mehedi Hasan <mehedi53423@gmail.com>
2023-02-04 00:18:09 +0600

    339A - Helpful Maths Problem Solved

---
+++ 339A-HelpfulMaths.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+
+namespace HelpfulMaths
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = string.Concat(Console.ReadLine().Replace("+", "").OrderBy(x => x));
+            string a = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i == s.Length - 1)
+                {
+                    a = a + s[i];
+                }
+                else
+                {
+                    a = a + s[i] + "+";
+                }
+            }
+
+            Console.WriteLine(a);
+        }
+    }
+}
