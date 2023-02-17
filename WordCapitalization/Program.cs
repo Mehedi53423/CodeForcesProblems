45b2cf952988fffcb64671f1f956009ea39262f7
Md. Mehedi Hasan <mehedi53423@gmail.com>
2023-02-18 03:44:37 +0600

    281A - Word Capitalization (Solved)

---
+++ WordCapitalization/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WordCapitalization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            Console.WriteLine(s.Substring(0, 1).ToUpper() + s.Substring(1, s.Length - 1));
+        }
+    }
+}
