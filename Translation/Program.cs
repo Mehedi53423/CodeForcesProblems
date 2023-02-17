42641559bfb1011f5ef66fb703cdab1055a784ee
Md. Mehedi Hasan <mehedi53423@gmail.com>
2023-02-18 04:29:51 +0600

    41A - Translation (Solved)

---
+++ Translation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Translation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string t = Console.ReadLine();
+            string a = "";
+
+            for(int i = s.Length-1; i >=0; i--)
+            {
+                a += s[i];
+            }
+
+            if(t == a)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
