4297a4334d13f50454f048f777e5f15f2cc8a1ad
Md. Mehedi Hasan <mehedi53423@gmail.com>
2023-02-18 03:22:21 +0600

    158A - Next Round (Solved)

---
+++ NextRound/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NextRound
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] s = Console.ReadLine().Split(' ');
+            string [] a = Console.ReadLine().Split(' ');
+            int p = 0;
+
+            for (int i = 0; i < Convert.ToInt32(s[0]); i++)
+            {
+                if (Convert.ToInt32(a[i]) >= Convert.ToInt32(a[Convert.ToInt32(s[1])-1]) && Convert.ToInt32(a[i]) > 0)
+                {
+                    p++;
+                }
+            }
+            Console.WriteLine(p);
+        }
+    }
+}
