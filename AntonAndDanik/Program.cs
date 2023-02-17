2a55c6198ffc0bc852944cb0dc61540d3d8a6a0f
Md. Mehedi Hasan <mehedi53423@gmail.com>
2023-02-18 04:14:43 +0600

    734A - Anton And Danik (Solved)

---
+++ AntonAndDanik/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AntonAndDanik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string s = Console.ReadLine();
+
+            int a = 0, d = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                if (s[i] == 'A')
+                {
+                    a++;
+                }
+                else
+                {
+                    d++;
+                }
+            }
+
+            if(a > d)
+            {
+                Console.WriteLine("Anton");
+            }
+            else if(a == d)
+            {
+                Console.WriteLine("Friendship");
+            }
+            else
+            {
+                Console.WriteLine("Danik");
+            }
+        }
+    }
+}
