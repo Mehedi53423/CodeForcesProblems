907d7d303ede4af77441d6568dcf23385ec40425
Md. Mehedi Hasan <mehedi53423@gmail.com>
2023-02-18 03:58:47 +0600

    110A - Nearly Lucky Number (Solved)

---
+++ NearlyLuckyNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NearlyLuckyNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int a = 0;
+
+            for(int i = 0; i< s.Length; i++)
+            {
+                if (s[i] == '4' || s[i] == '7')
+                {
+                    a++;
+                }
+            }
+
+            if(a == 4 || a == 7)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
