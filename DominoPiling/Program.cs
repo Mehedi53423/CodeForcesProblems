0580afcb784e2db456b58818ca5dac7fa83d8c0c
Md. Mehedi Hasan <mehedi53423@gmail.com>
2023-02-18 02:46:26 +0600

    50A - Domino Piling (Solved)

---
+++ DominoPiling/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DominoPiling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] s = Console.ReadLine().Split(' ');
+
+            Console.WriteLine((Convert.ToInt32(s[0]) * Convert.ToInt32(s[1])) / 2);
+        }
+    }
+}
---
+++ DominoPiling/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DominoPiling")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DominoPiling")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0051e28e-5bec-48d1-b0d2-7d6260737f89")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
