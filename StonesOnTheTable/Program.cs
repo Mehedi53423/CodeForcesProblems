1a0cc7f012dda0b4cbfa09e9f06480bf9cb3a91f
Md. Mehedi Hasan <mehedi53423@gmail.com>
2023-05-06 00:47:36 +0600

    266A - Stones on the Table (Solved)

---
+++ StonesOnTheTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StonesOnTheTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string s = Console.ReadLine();
+            int a = 0;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (s[i] == s[i + 1])
+                {
+                    a++;
+                }
+            }
+
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ StonesOnTheTable/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StonesOnTheTable")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StonesOnTheTable")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("980a0cf4-9730-4cb2-b97d-3b844ff5e518")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
