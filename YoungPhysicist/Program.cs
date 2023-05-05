03a1132094977c0fc9d399d1b2bea8bbad7c87f0
Md. Mehedi Hasan <mehedi53423@gmail.com>
2023-05-06 00:35:09 +0600

    282A - Bit++ (Solved)

---
+++ YoungPhysicist/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace YoungPhysicist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            //int 
+        }
+    }
+}
